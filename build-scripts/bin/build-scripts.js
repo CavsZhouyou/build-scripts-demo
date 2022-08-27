fe2934ab54750b4e2f098b0d3f395c8bf149923e
xiaotian.zy <xiaotian.zy@alibaba-inc.com>
2022-08-27 22:02:05 +0800

    feat(build-scripts/package.json): 添加 bulid-scripts

---
+++ build-scripts/bin/build-scripts.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const build = require('../src/commands/build');
+
+(async () => {
+  // 命令注册
+  program.command('build').description('build project').action(build);
+
+  // 判断是否有存在运行的命令，如果有则退出已执行命令
+  const proc = program.runningCommand;
+  if (proc) {
+    proc.on('close', process.exit.bind(process));
+    proc.on('error', () => {
+      process.exit(1);
+    });
+  }
+
+  // 命令行参数解析
+  program.parse(process.argv);
+
+  // 如果无子命令，展示 help 信息
+  const subCmd = program.args[0];
+  console.log('program.args: ', program.args);
+  if (!subCmd) {
+    program.help();
+  }
+})();
---
+++ build-scripts/src/commands/build.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = async () => {
+  const rootDir = process.cwd();
+
+  // 定义 webpack 配置
+  const config = {
+    entry: path.resolve(rootDir, './src/index.ts'),
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(rootDir, './dist'),
+    },
+  };
+
+  // 实例化 webpack
+  const compiler = webpack(config);
+
+  // 执行 webpack 编译
+  compiler.run((err, stats) => {
+    compiler.close((closeErr) => {});
+  });
+};

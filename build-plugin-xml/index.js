946fa18a9e71346b92c5fcf42e2522376060a814
xiaotian.zy <xiaotian.zy@alibaba-inc.com>
2022-09-09 17:16:35 +0800

    feat: 更新配置

--- build-plugin-xml/index.js
+++ build-plugin-xml/index.js
@@ -2,6 +2,7 @@ module.exports = async ({ onGetWebpackConfig }) => {
   onGetWebpackConfig('base', (webpackConfig) => {
     webpackConfig.module
       .rule('xml')
+      .before('ts')
       .test(/\.xml$/i)
       .use('xml-loader')
       .loader(require.resolve('xml-loader'));
